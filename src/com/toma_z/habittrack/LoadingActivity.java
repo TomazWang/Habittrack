40608ea9b516d681562456ea69174bdadbff56b0
TomazWang.pc <TomzWang.tw@gmail.com>
2014-09-17 15:28:44 +0800

    Create basic activity 	- MainActivity and LoadingActivity created 	- modify Manifest to set launch activity
    Signed-off-by: TomazWang.pc <TomzWang.tw@gmail.com>

---
+++ src/com/toma_z/habittrack/LoadingActivity.java
@@ -0,0 +1,26 @@
+package com.toma_z.habittrack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoadingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		showtitles();
+		nextPage();
+	}
+
+	private void nextPage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void showtitles() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/com/toma_z/habittrack/MainActivity.java
@@ -0,0 +1,26 @@
+package com.toma_z.habittrack;
+
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		initActionBar();
+		initWidgets();
+	}
+
+	private void initWidgets() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initActionBar() {
+		// TODO Auto-generated method stub
+		
+	}
+}	
