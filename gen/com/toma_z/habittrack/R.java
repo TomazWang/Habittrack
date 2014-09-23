0348fd0d041738f18f061e0359e0cb6de2612c13
TomazWang.pc <TomzWang.tw@gmail.com>
2014-09-24 01:02:54 +0800

    Fixing gitignore

--- gen/com/toma_z/habittrack/R.java
+++ gen/com/toma_z/habittrack/R.java
@@ -15,11 +15,17 @@ public final class R {
     }
     public static final class id {
         public static final int editText1=0x7f060000;
-        public static final int frame_calendar=0x7f060005;
-        public static final int frame_detail=0x7f060004;
-        public static final int frame_summery=0x7f060003;
+        public static final int frame_calendar=0x7f06000b;
+        public static final int frame_detail=0x7f06000a;
+        public static final int frame_summery=0x7f060009;
         public static final int img_face=0x7f060001;
-        public static final int list_habit=0x7f060006;
+        public static final int list_habit=0x7f06000c;
+        public static final int progressBar_taskComplete=0x7f060008;
+        public static final int txt_day1=0x7f060003;
+        public static final int txt_day2=0x7f060004;
+        public static final int txt_day3=0x7f060005;
+        public static final int txt_day4=0x7f060006;
+        public static final int txt_day5=0x7f060007;
         public static final int txt_name=0x7f060002;
     }
     public static final class layout {
