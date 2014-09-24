98212639fa2edd8ae58237462f2e01620ac30044
Tomaz Wang@VAIO <TomazWang.tw@gmail.com>
2014-09-24 16:54:30 +0800

    Build LaodingAcitivity.java
    Signed-off-by: Tomaz Wang@VAIO <TomazWang.tw@gmail.com>

--- src/com/toma_z/habittrack/LoadingActivity.java
+++ src/com/toma_z/habittrack/LoadingActivity.java
@@ -1,26 +1,64 @@
 package com.toma_z.habittrack;
 
+import android.R.anim;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
 
 public class LoadingActivity extends Activity {
+
+	private final int delayMillis = 1000; // delay between fade_in and nextPage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.page_loading);
 		showtitles();
-		nextPage();
 	}
 
 	private void nextPage() {
-		// TODO Auto-generated method stub
-		
+		startActivity(new Intent().setClass(LoadingActivity.this,
+				MainActivity.class));
 	}
 
 	private void showtitles() {
-		// TODO Auto-generated method stub
-		
+		final Animation fade_in = AnimationUtils.loadAnimation(this,
+				anim.fade_in);
+
+		fade_in.setAnimationListener(new AnimationListener() {
+
+			@Override
+			public void onAnimationStart(Animation animation) {
+
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				Handler timeDelay = new Handler();
+				timeDelay.postDelayed(new Runnable() {
+
+					@Override
+					public void run() {
+						nextPage();
+					}
+				}, delayMillis);
+			}
+		});
+
+		View view = this.findViewById(android.R.id.content);
+		view.setAlpha(0f);
+		view.startAnimation(fade_in);
+
 	}
-	
 }
