270a3c7b3657d9b13bca24c2698429a7136fdb3b
Tomaz Wang@VAIO <TomazWang.tw@gmail.com>
2014-09-24 17:10:55 +0800

    Build base MainActivity
    Signed-off-by: Tomaz Wang@VAIO <TomazWang.tw@gmail.com>

--- src/com/toma_z/habittrack/MainActivity.java
+++ src/com/toma_z/habittrack/MainActivity.java
@@ -5,22 +5,26 @@ import android.app.ActionBar;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
+
+	private ActionBar actionBar;
+	
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.page_main);
 		initActionBar();
 		initWidgets();
 	}
 
 	private void initWidgets() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void initActionBar() {
-		// TODO Auto-generated method stub
-		
+		actionBar = getActionBar();
 	}
-}	
+}
